a12452ad640246745491659ca9807293eadcde47
Sarang Sharma <sarangchamp2--4>
2023-03-05 23:48:52 +0530

    cc

--- src/components/Buttonlist.js
+++ src/components/Buttonlist.js
@@ -5,13 +5,8 @@ const list =["All","Sports","Valorant","Les Twins","Comedy","Marvel","Cars","Com
 
 const Buttonlist = () => {
   return (
-<<<<<<< HEAD
     <div className="flex">
     {list.map((name)=>(<button key={name.id} className='px-4 py-1 m-3 bg-gray-200 rounded-lg'>{name}</button>))}
-=======
-    <div>
-      Buttonlist to new
->>>>>>> 96ebe44e12e48ec1a079928fb16b822fc147e00b
     </div>
   );
 };
